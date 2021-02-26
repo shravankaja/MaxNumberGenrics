f44deffb6f8bb8cf0d67228b6e907ae74942319b
shravan-kaja <shravan.kaja@gmail.com>
2021-02-26 20:34:24 +0530

    Added: method to find max number with Float Object

--- src/main/java/com/maxnumber/MaxNumber.java
+++ src/main/java/com/maxnumber/MaxNumber.java
@@ -14,8 +14,27 @@ public class MaxNumber {
         return max;
     }
 
+    public static Float findFloatMaxNumber(Float firstFloatNumber, Float secondFloatNumber, Float thirdFloatNumber) {
+        Float max = firstFloatNumber;
+        if (secondFloatNumber.compareTo(max) > 0) {
+            max = secondFloatNumber;
+        }
+        if ((thirdFloatNumber.compareTo(max)) > 0) {
+            max = thirdFloatNumber;
+        }
+        printMax(firstFloatNumber, secondFloatNumber, thirdFloatNumber, max);
+        return max;
+    }
+
+
     public static void printMax(Integer firstNumber, Integer secondNumber, Integer thirdNumber, Integer max) {
-        System.out.println("Enter numbers are : " + firstNumber + " " + secondNumber + " " + thirdNumber + " Max is" + max);
+        System.out.println("Enter Integers numbers are : " + firstNumber + " " + secondNumber + " " + thirdNumber + " Max is" + max);
+
+    }
+
+    public static void printMax(Float firstNumberFloat, Float secondNumberFloat, Float thirdNumberFloat, Float max) {
+        System.out.println("Enter Float numbers are : " + firstNumberFloat + " " + secondNumberFloat + " " +
+                thirdNumberFloat + " Max is" + max);
 
     }
 
@@ -25,5 +44,11 @@ public class MaxNumber {
         Integer secondNumber = Integer.valueOf("20");
         Integer thirdNumber = Integer.valueOf("100");
         findMaxNumber(firstNumber, secondNumber, thirdNumber);
+        Float firstNumberFloat = Float.valueOf("24.9");
+        Float secondNumberFloat = Float.valueOf("10.7");
+        Float thirdNumberFloat = Float.valueOf("69.4");
+        findFloatMaxNumber(firstNumberFloat,secondNumberFloat,thirdNumberFloat);
     }
+
+
 }
