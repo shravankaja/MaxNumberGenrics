d635d36b8c727945abc5b9a3aa68bfe6b0cb02b4
shravan-kaja <shravan.kaja@gmail.com>
2021-02-26 16:36:28 +0530

    Added: used Integer Objects to get max number

--- src/main/java/com/maxnumber/MaxNumber.java
+++ src/main/java/com/maxnumber/MaxNumber.java
@@ -1,7 +1,29 @@
 package com.maxnumber;
 
 public class MaxNumber {
+
+    public static Integer findMaxNumber(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer max = firstNumber;
+        if (secondNumber.compareTo(max) > 0) {
+            max = secondNumber;
+        }
+        if ((thirdNumber.compareTo(max)) > 0) {
+            max = thirdNumber;
+        }
+        printMax(firstNumber, secondNumber, thirdNumber, max);
+        return max;
+    }
+
+    public static void printMax(Integer firstNumber, Integer secondNumber, Integer thirdNumber, Integer max) {
+        System.out.println("Enter numbers are : " + firstNumber + " " + secondNumber + " " + thirdNumber + " Max is" + max);
+
+    }
+
     public static void main(String Args[]) {
         System.out.println("Welcome to Max Number Program");
+        Integer firstNumber = Integer.valueOf("199");
+        Integer secondNumber = Integer.valueOf("20");
+        Integer thirdNumber = Integer.valueOf("100");
+        findMaxNumber(firstNumber, secondNumber, thirdNumber);
     }
 }
