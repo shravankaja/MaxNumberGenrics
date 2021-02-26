0ecc0a0d0a6a3654b1109dd6b6abecd81f157fd7
shravan-kaja <shravan.kaja@gmail.com>
2021-02-26 20:35:03 +0530

    Added: test case for float object number

--- src/test/java/com/maxnumber/MaxNumberTest.java
+++ src/test/java/com/maxnumber/MaxNumberTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.*;
 public class MaxNumberTest {
     MaxNumber maxNumber = new MaxNumber();
     Integer resultNumber;
+    Float resultFloatNumber;
 
     @Test
     public void maxNumberFirstPosition() {
-        resultNumber = maxNumber.findMaxNumber(199, 22, 1);
+        resultNumber = maxNumber.findMaxNumber(199, 9, 145);
         Assertions.assertEquals(199, resultNumber);
     }
 
@@ -22,5 +23,14 @@ public class MaxNumberTest {
     public void maxNumberThirdPosition() {
         resultNumber = maxNumber.findMaxNumber(147, 97, 360);
         Assertions.assertEquals(360, resultNumber);
+    }
+
+    @Test
+    public void maxFloatNumberTest() {
+        Float firstNumberFloat = Float.valueOf("24.9");
+        Float secondNumberFloat = Float.valueOf("10.7");
+        Float thirdNumberFloat = Float.valueOf("69.4");
+        resultFloatNumber = maxNumber.findFloatMaxNumber(firstNumberFloat,secondNumberFloat,thirdNumberFloat);
+        Assertions.assertEquals(thirdNumberFloat, resultFloatNumber);
     }
 }
