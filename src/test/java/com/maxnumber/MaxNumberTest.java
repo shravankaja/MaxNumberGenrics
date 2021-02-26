086da807f99838600cdf32261471422b4e9cab29
shravan-kaja <shravan.kaja@gmail.com>
2021-02-26 20:57:16 +0530

    Added: test case to test max string in all three positions

--- src/test/java/com/maxnumber/MaxNumberTest.java
+++ src/test/java/com/maxnumber/MaxNumberTest.java
@@ -6,6 +6,7 @@ public class MaxNumberTest {
     MaxNumber maxNumber = new MaxNumber();
     Integer resultNumber;
     Float resultFloatNumber;
+    String resultString;
 
     @Test
     public void maxNumberFirstPosition() {
@@ -26,11 +27,47 @@ public class MaxNumberTest {
     }
 
     @Test
-    public void maxFloatNumberTest() {
-        Float firstNumberFloat = Float.valueOf("24.9");
+    public void maxFloatNumberFirstPositionTest() {
+        Float firstNumberFloat = Float.valueOf("124.9");
         Float secondNumberFloat = Float.valueOf("10.7");
         Float thirdNumberFloat = Float.valueOf("69.4");
-        resultFloatNumber = maxNumber.findFloatMaxNumber(firstNumberFloat,secondNumberFloat,thirdNumberFloat);
+        resultFloatNumber = maxNumber.findFloatMaxNumber(firstNumberFloat, secondNumberFloat, thirdNumberFloat);
+        Assertions.assertEquals(firstNumberFloat, resultFloatNumber);
+    }
+
+    @Test
+    public void maxFloatNumberSecondPositionTest() {
+        Float firstNumberFloat = Float.valueOf("24.9");
+        Float secondNumberFloat = Float.valueOf("101.7");
+        Float thirdNumberFloat = Float.valueOf("69.4");
+        resultFloatNumber = maxNumber.findFloatMaxNumber(firstNumberFloat, secondNumberFloat, thirdNumberFloat);
+        Assertions.assertEquals(secondNumberFloat, resultFloatNumber);
+    }
+
+    @Test
+    public void maxFloatNumberThirdPositionTest() {
+        Float firstNumberFloat = Float.valueOf("11.9");
+        Float secondNumberFloat = Float.valueOf("110.7");
+        Float thirdNumberFloat = Float.valueOf("169.4");
+        resultFloatNumber = maxNumber.findFloatMaxNumber(firstNumberFloat, secondNumberFloat, thirdNumberFloat);
         Assertions.assertEquals(thirdNumberFloat, resultFloatNumber);
+    }
+
+    @Test
+    public void maxStringFirstPositionTest() {
+        resultString = maxNumber.findMaxString("wa", "h", "s");
+        Assertions.assertEquals("wa", resultString);
+    }
+
+    @Test
+    public void maxStringSecondPositionTest() {
+        resultString = maxNumber.findMaxString("h", "wa", "s");
+        Assertions.assertEquals("wa", resultString);
+    }
+
+    @Test
+    public void maxStringThirdPositionTest() {
+        resultString = maxNumber.findMaxString("w", "h", "wa");
+        Assertions.assertEquals("wa", resultString);
     }
 }
