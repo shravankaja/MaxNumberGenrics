1674fe8ca6e98204ab25042dedb66c5d751b7ed2
shravan-kaja <shravan.kaja@gmail.com>
2021-02-27 16:18:13 +0530

    Added: genric print method

--- src/main/java/com/maxnumber/MaxNumber.java
+++ src/main/java/com/maxnumber/MaxNumber.java
@@ -2,71 +2,64 @@ package com.maxnumber;
 
 public class MaxNumber<T extends Comparable> {
 
-    public T[] elements;
-    public T elementsTest;
+	public T[] elements;
+	public T elementsTest;
 
-    public MaxNumber(T... elements) {
-        for (T e : elements) {
-            this.elements = elements;
-        }
-    }
+	public MaxNumber(T... elements) {
+		for (T e : elements) {
+			this.elements = elements;
+		}
+	}
 
-    public <T> void maximumArray() {
+	public <T> void maximumArray() {
 
-        MaxNumber.findMaxGenric(elements);
-    }
+		MaxNumber.findMaxGenric(elements);
+	}
 
-    public static <T extends Comparable<T>> T findMaxGenric(T elements[]) {
-        T max = null;
-        int n = elements.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if ((elements[j].compareTo(elements[j + 1])) > 0) {
-                    T temp = elements[j];
-                    elements[j] = elements[j + 1];
-                    elements[j + 1] = temp;
-                }
-            }
-        }
+	public static <T extends Comparable<T>> T findMaxGenric(T elements[]) {
+		T max = null;
+		int n = elements.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if ((elements[j].compareTo(elements[j + 1])) > 0) {
+					T temp = elements[j];
+					elements[j] = elements[j + 1];
+					elements[j + 1] = temp;
+				}
+			}
+		}
+		printMaxGeneric(elements);
+		return elements[n - 1];
+	}
 
-        System.out.println("Sorted List");
-        for (T a : elements) {
-            System.out.println(a);
-        }
-        System.out.println("Max element is : " + elements[n - 1]);
-        return elements[n - 1];
-    }
+	public static <T extends Comparable<T>> T findMaxGenricTest(T... elementsTest) {
+		T max = null;
+		int n = elementsTest.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if ((elementsTest[j].compareTo(elementsTest[j + 1])) > 0) {
+					T temp = elementsTest[j];
+					elementsTest[j] = elementsTest[j + 1];
+					elementsTest[j + 1] = temp;
+				}
+			}
+		}
+		return elementsTest[n - 1];
+	}
 
-    public static <T extends Comparable<T>> T findMaxGenricTest(T... elementsTest) {
-        T max = null;
-        int n = elementsTest.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if ((elementsTest[j].compareTo(elementsTest[j + 1])) > 0) {
-                    T temp = elementsTest[j];
-                    elementsTest[j] = elementsTest[j + 1];
-                    elementsTest[j + 1] = temp;
-                }
-            }
-        }
+	public static <T> void printMaxGeneric(T elements[]) {
+		int n = elements.length;
+		System.out.println("Sorted List of Elements : ");
+		for (T a : elements) {
+			System.out.println(a);
+		}
+		System.out.println("Max element in the list is : " + elements[n - 1]);
+	}
 
-        System.out.println("Sorted List");
-        for (T a : elementsTest) {
-             
-        }
-        System.out.println("Max element is : " + elementsTest[n - 1]);
-        return elementsTest[n - 1];
-    }
-
-    public static <T> void printMaxGeneric(T firstElement, T secondElement, T thirdElement, T max) {
-        System.out.println(
-                "Enter Elements are : " + firstElement + " " + secondElement + " " + thirdElement + " Max is :" + max);
-    }
-
-    public static <T> void main(String Args[]) {
-        MaxNumber<Integer> maxNumber = new MaxNumber(1000, 5, 9, 8, 7, 52, 63, 962, 2, 59, 5);
-        MaxNumber<String> maxString = new MaxNumber("Apple", "Bannnaa", "Peacha", "Peach");
-        maxString.maximumArray();
-        maxNumber.maximumArray();
-    }
+	public static <T> void main(String Args[]) {
+		MaxNumber<Integer> maxNumber = new MaxNumber(1000, 5, 9, 8, 7, 52, 63, 962, 2, 59, 5);
+		MaxNumber<String> maxString = new MaxNumber("Apple", "Bannnaa", "Peacha", "Peach");
+		maxString.maximumArray();
+		maxNumber.maximumArray();
+	}
 }
